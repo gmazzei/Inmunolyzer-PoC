31f21804ee659ac346aa84830b71115335131ef2
gmazzei <gmazzei91@gmail.com>
2015-06-08 15:53:52 -0300

    Login

--- src/main/java/com/syslab/LoginPage.java
+++ src/main/java/com/syslab/LoginPage.java
@@ -2,10 +2,10 @@ package com.syslab;
 
 import org.apache.wicket.markup.html.WebPage;
 
-public class HomePage extends WebPage {
+public class LoginPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-	public HomePage() {
-
+	public LoginPage() {
+		
 	}
 }
--- src/main/java/com/syslab/WicketApplication.java
+++ src/main/java/com/syslab/WicketApplication.java
@@ -3,31 +3,19 @@ package com.syslab;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
- * 
- * @see com.syslab.Start#main(String[])
- */
-public class WicketApplication extends WebApplication
-{
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
+public class WicketApplication extends WebApplication {
+
 	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
-		return HomePage.class;
+	public Class<? extends WebPage> getHomePage() {
+		return LoginPage.class;
 	}
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init()
-	{
+	public void init() {
 		super.init();
-
-		// add your configuration here
+		
+		this.mountPage("LoginPage", LoginPage.class);
+		this.mountPage("HomePage", HomePage.class);
+		
+		//TODO: Hash URL
 	}
 }
